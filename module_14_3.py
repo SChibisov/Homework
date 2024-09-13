761e969a844596d825193ddbe74e44424970d53e
SChibisov <168549074+SChibisov@users.noreply.github.com>
2024-09-13 20:41:15 +0300

    module_14_3.py

--- module_14_3.py
+++ module_14_3.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = '7480441215:AAEeuCfWnaXWDRaZGLu5uopsGGUojDrqPL4'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
